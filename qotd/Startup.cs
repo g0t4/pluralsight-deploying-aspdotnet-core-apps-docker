a9dd429d749eacedb1b82f48415df310426bb281
Wes Higbee <wes.mcclure@gmail.com>
2017-05-01 17:42:03 -0400

    Add xml content support

--- qotd/Startup.cs
+++ qotd/Startup.cs
@@ -28,7 +28,8 @@ namespace qotd
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc()
+                .AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
